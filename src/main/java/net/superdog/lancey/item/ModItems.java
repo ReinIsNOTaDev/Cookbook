629efccb902ddce83ec37109089f7e8af41d696d
Super1166 <110970817+Super1166@users.noreply.github.com>
2022-12-24 17:12:04 +0200

    Ice Cream shenanigans WAAAAAA

--- src/main/java/net/superdog/lancey/item/ModItems.java
+++ src/main/java/net/superdog/lancey/item/ModItems.java
@@ -10,8 +10,17 @@ public class ModItems {
     public static final Item CHOCOLATE = registerItem("chocolate",
             new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build()).group(ModItemGroup.LANCEYS_MOD)));
 
-    public static final Item ICE_CREAM = registerItem("ice_cream",
-            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(8).saturationModifier(0.5f).build()).group(ModItemGroup.LANCEYS_MOD)));
+    public static final Item MIXED_ICE_CREAM = registerItem("mixed_ice_cream",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(10).saturationModifier(0.8f).build()).group(ModItemGroup.LANCEYS_MOD)));
+
+    public static final Item CHOCO_ICE_CREAM = registerItem("choco_ice_cream",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(7).saturationModifier(0.3f).build()).group(ModItemGroup.LANCEYS_MOD)));
+
+    public static final Item CREAMY_ICE_CREAM = registerItem("creamy_ice_cream",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(7).saturationModifier(0.3f).build()).group(ModItemGroup.LANCEYS_MOD)));
+
+    public static final Item SWEET_BERRY_ICE_CREAM = registerItem("sweet_berry_ice_cream",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(7).saturationModifier(0.3f).build()).group(ModItemGroup.LANCEYS_MOD)));
 
     public static final Item CHOCO_COOKIE = registerItem("choco_cookie",
             new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(4).saturationModifier(0.1f).snack().build()).group(ModItemGroup.LANCEYS_MOD)));

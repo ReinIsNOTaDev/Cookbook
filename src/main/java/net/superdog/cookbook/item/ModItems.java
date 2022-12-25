40c67d2a2cdc1bdb82373c9c4ea2a67ad288f831
Super1166 <110970817+Super1166@users.noreply.github.com>
2022-12-25 22:33:20 +0200

    Added scrambled eggs and fried egg Yay version 1 is done WOOOO

--- src/main/java/net/superdog/cookbook/item/ModItems.java
+++ src/main/java/net/superdog/cookbook/item/ModItems.java
@@ -7,17 +7,23 @@ import net.minecraft.util.registry.Registry;
 import net.superdog.cookbook.cookbookMod;
 
 public class ModItems {
+
+    public static final Item DOUGH = registerItem("dough",
+            new Item(new FabricItemSettings().group(ModItemGroup.COOKBOOK_MOD)));
     public static final Item CHOCOLATE = registerItem("chocolate",
             new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build()).group(ModItemGroup.COOKBOOK_MOD)));
-
-    public static final Item MIXED_ICE_CREAM = registerItem("ice_cream",
-            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(10).saturationModifier(0.8f).build()).group(ModItemGroup.COOKBOOK_MOD)));
 
     public static final Item CHOCO_COOKIE = registerItem("choco_cookie",
             new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(4).saturationModifier(0.1f).snack().build()).group(ModItemGroup.COOKBOOK_MOD)));
 
-    public static final Item DOUGH = registerItem("dough",
-            new Item(new FabricItemSettings().group(ModItemGroup.COOKBOOK_MOD)));
+    public static final Item MIXED_ICE_CREAM = registerItem("ice_cream",
+            new StewItem(new FabricItemSettings().food(new FoodComponent.Builder().hunger(10).saturationModifier(0.8f).build()).group(ModItemGroup.COOKBOOK_MOD).maxCount(1)));
+
+    public static final Item FRIED_EGG = registerItem("fried_egg",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(5).saturationModifier(0.4F).build()).group(ModItemGroup.COOKBOOK_MOD)));
+
+    public static final Item SCRAMBLED_EGGS = registerItem("scrambled_eggs",
+            new StewItem(new FabricItemSettings().food(new FoodComponent.Builder().hunger(7).saturationModifier(0.8f).build()).group(ModItemGroup.COOKBOOK_MOD).maxCount(1)));
 
 
     private static Item registerItem(String name, Item item) {
